bb075de87b3b35397ca5ac909679b8cb7bf25a5d
Clupai8o0 <clupaio4@gmail.com>
2023-08-12 20:48:00 +0300

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useRef } from "react";
+
+import DisplaySection from "./components/display-section";
+import Jumbotron from "./components/jumbotron";
+import Navbar from "./components/nav";
+import SoundSection from "./components/sound-section";
+import WebGIViewer from "./components/webgi-viewer";
+import Loader from "./components/loader";
 
 function App() {
+	const webgiViewerRef = useRef(null);
+	const contentRef = useRef(null);
 
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+	const handlePreview = () => {
+		webgiViewerRef.current.triggerPreview();
+	};
+
+	return (
+		<div className="App">
+			<Loader />
+			<div id="content" ref={contentRef}>
+				<Navbar />
+				<Jumbotron />
+				<SoundSection />
+				<DisplaySection triggerPreview={handlePreview} />
+			</div>
+			<WebGIViewer ref={webgiViewerRef} contentRef={contentRef} />
+		</div>
+	);
 }
 
 export default App;
